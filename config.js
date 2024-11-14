14de5bd009a7279021bfc654e1c99a00f5d7bf60
Bsambsnsizj <bsamggg7@gmail.com>
2024-11-14 03:43:07 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-clientId: "", // id bot
-    guildId: "", // id server
-    token: "" //token bot
+clientId: "1306405094849511554", // id bot
+    guildId: "1282030826795827353", // id server
+    token: "MTMwNjQwNTA5NDg0OTUxMTU1NA.GZufke.jKVoAwSZ9axDqd43Qs2vp3YJAAQtrif43zSumE" //token bot
 }
